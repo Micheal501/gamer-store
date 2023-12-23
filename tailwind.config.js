98fb3a383f94cb3522b989eda1a5d4856a707953
Micheal Owen <mowen8983@gmail.com>
2023-12-23 09:58:58 +0700

    gamer-store

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        Background: '#232946',
+        Headline: '#fffffe',
+        Paragraph: '#b8c1ec',
+      },
+    },
+  },
+  plugins: [],
+};
